9e5ba946d723f0fe96075d272ee115bca76dd64b
saruj <14skd12@gmail.com>
2025-11-30 19:31:06 +0530

    changes

--- app/src/main/java/com/saruj/snakeandladder/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/saruj/snakeandladder/ui/screens/SplashScreen.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.delay
 fun SplashScreen(navController: NavController) {
     LaunchedEffect(Unit) {
 
+
         delay(3000) // 3 seconds
         navController.navigate("dashboard") {
             popUpTo("splash") { inclusive = true }
