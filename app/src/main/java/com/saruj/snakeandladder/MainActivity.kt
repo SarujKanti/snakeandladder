46cda05dbf5795232b7a47a85f9aec70afbfe253
saruj <14skd12@gmail.com>
2025-12-12 14:36:03 +0530

    changes

--- app/src/main/java/com/saruj/snakeandladder/MainActivity.kt
+++ app/src/main/java/com/saruj/snakeandladder/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "SARUJ DAS $name!",
+        text = "SARUJ $name!",
         modifier = modifier
     )
 }
