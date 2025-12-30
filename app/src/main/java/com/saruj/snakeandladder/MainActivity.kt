9e34490c33b3e0ab7feb2750d27ea98e0690fa2e
saruj <14skd12@gmail.com>
2025-12-30 10:15:04 +0530

    screen changes in mainActivity

--- app/src/main/java/com/saruj/snakeandladder/MainActivity.kt
+++ app/src/main/java/com/saruj/snakeandladder/MainActivity.kt
@@ -26,7 +26,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             NavHost(navController = navController, startDestination = "splash") {
-                composable("splash") { SplashScreen(navController) }
+                composable("splash")
+                { SplashScreen(navController) }
                 composable("dashboard") { DashboardScreen(navController) }
                 composable("gameBoard") { GameBoardScreen() }
             }
@@ -42,6 +43,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 @Preview(showBackground = true)
+
 
 @Composable
 fun GreetingPreview() {
