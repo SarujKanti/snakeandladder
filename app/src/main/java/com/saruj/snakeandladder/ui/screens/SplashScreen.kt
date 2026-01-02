4ffd278cdac7574c49f561b872ba535c72587874
saruj <14skd12@gmail.com>
2026-01-02 16:40:30 +0530

    added function GreetingPreview in mainActivity

--- app/src/main/java/com/saruj/snakeandladder/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/saruj/snakeandladder/ui/screens/SplashScreen.kt
@@ -23,7 +23,6 @@ fun SplashScreen(navController: NavController) {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     )
-
     {
         Text(
             text = "🐍 Snake & Ladder 🎲",
