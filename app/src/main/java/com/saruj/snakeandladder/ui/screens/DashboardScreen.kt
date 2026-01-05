78451055bf999ed462e7c1c6bd0bbdb07eb0cbd0
saruj <14skd12@gmail.com>
2026-01-05 10:38:22 +0530

    in Dashboard changed permission

--- app/src/main/java/com/saruj/snakeandladder/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/saruj/snakeandladder/ui/screens/DashboardScreen.kt
@@ -30,6 +30,7 @@ fun DashboardScreen(navController: NavController) {
             }
             Button(
                 onClick = { /* TODO: Rules Screen */ },
+
                 modifier = Modifier.padding(10.dp)
             ) {
                 Text("📜 Rules")
