d7e1f9a06108d27a61271e91b43c238cd6e07b3c
saruj <14skd12@gmail.com>
2026-01-20 00:23:13 +0530

    in Dashboard changed permission

--- app/src/main/java/com/saruj/snakeandladder/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/saruj/snakeandladder/ui/screens/DashboardScreen.kt
@@ -25,7 +25,6 @@ fun DashboardScreen(navController: NavController) {
             )
             Button(
                 onClick = { navController.navigate("gameBoard") },
-
                 modifier = Modifier.padding(8.dp)
             )
             {
