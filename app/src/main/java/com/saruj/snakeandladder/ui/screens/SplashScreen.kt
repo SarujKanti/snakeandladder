877d3b3720b2ab4957564d3a283dc334331d58a8
saruj <14skd12@gmail.com>
2026-02-05 00:00:50 +0530

    in Dashboard changed permission

--- app/src/main/java/com/saruj/snakeandladder/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/saruj/snakeandladder/ui/screens/SplashScreen.kt
@@ -27,6 +27,7 @@ fun SplashScreen(navController: NavController) {
         Text(
             text = "🐍 Snake & Ladder 🎲",
             fontSize = 40.sp,
+
             fontWeight = FontWeight.ExtraBold
         )
     }
